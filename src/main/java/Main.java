678a63ca739b20c5623ff5fd3bfb9571d5e51b95
Danielle <danielle@Zipcoders-MacBook-Pro-8.local>
2023-06-29 12:39:23 -0400

    I have made changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,34 @@
+import java.util.Scanner;
+import java.util.Random;
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
-        
+        Random danny = new Random();
+        int n = danny.nextInt(21);
+        int guess=0;
+        boolean beans= false;
+        System.out.println("Guess The Number");
+
+
+
+        while (beans == false) {
+            Scanner in = new Scanner(System.in);
+            guess= in.nextInt();
+            if (n < guess)
+            {
+                System.out.println("Too Large");
+            }
+            else if (n > guess) {
+                System.out.println("Too Small");
+            }
+            else
+            {
+                System.out.println("You Are Correct");
+                    beans = true;
+            }
+        }
     }
 }
